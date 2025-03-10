090920aa911e239b72e0b4a1fb533c89b9b01971
Đoàn Ngọc Minh Quân <minhquandoanngoc@gmail.com>
2025-03-10 18:50:42 +0700

    [Fix] Replace port for connection string

--- HMES.API/Program.cs
+++ HMES.API/Program.cs
@@ -49,7 +49,8 @@ var connectionString = rawConnectionString
     .Replace("${DB_SERVER}", Environment.GetEnvironmentVariable("DB_SERVER") ?? "")
     .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER") ?? "")
     .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "")
-    .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME") ?? "");
+    .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME") ?? "")
+    .Replace("${DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT") ?? "");
 
 builder.Services.AddDbContext<HmesContext>(options =>
     options.UseSqlServer(connectionString));
