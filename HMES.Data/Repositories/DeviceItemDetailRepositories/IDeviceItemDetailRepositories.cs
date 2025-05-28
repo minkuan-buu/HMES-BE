8290ae892d547c0f53b478e84e4cc3780a6b00a1
Thiện Nguyễn <thiennqse171671@fpt.edu.vn>
2025-05-28 15:20:31 +0700

    [FIX] CreateDeviceItem

---
+++ HMES.Data/Repositories/DeviceItemDetailRepositories/IDeviceItemDetailRepositories.cs
@@ -0,0 +1,9 @@
+using System;
+using HMES.Data.Entities;
+using HMES.Data.Repositories.GenericRepositories;
+
+namespace HMES.Data.Repositories.DeviceItemDetailRepositories;
+
+public interface IDeviceItemDetailRepositories: IGenericRepositories<DeviceItemDetail>
+{
+}
