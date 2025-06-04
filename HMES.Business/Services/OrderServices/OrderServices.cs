e637c7df51a8f53289e6ed50d6b6b4463cb74393
Thiện Nguyễn <thiennqse171671@fpt.edu.vn>
2025-06-04 11:15:36 +0700

    [FIX] CreateOrder

--- HMES.Business/Services/OrderServices/OrderServices.cs
+++ HMES.Business/Services/OrderServices/OrderServices.cs
@@ -317,6 +317,7 @@ namespace HMES.Business.Services.OrderServices
                     if (device.Quantity < devReq.Quantity)
                         throw new CustomException($"Thiết bị {TextConvert.ConvertFromUnicodeEscape(device.Name)} không đủ số lượng trong kho.");
                 }
+                
                 // Xác định orderId
                 Guid orderId = existingOrder != null ? existingOrder.Id : Guid.NewGuid();
 
